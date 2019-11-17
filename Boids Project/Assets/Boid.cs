88bce46467011ab34faca979f281d899062fd0a8
Hugo Moran <dev.hugomoran@gmail.com>
2019-11-17 15:05:10 -0300

    Created the Boid class and filled Spawner class.

--- Boids Project/Assets/Boid.cs
+++ Boids Project/Assets/Boid.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set Dynamically")]
+    public Rigidbody rigid;
+    
+    // Use this for initialization
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+
+        // Set a random initial position
+        pos = Random.insideUnitSphere * Spawner.S.spawnRadius;
+
+        // Set a random initial velocity
+        Vector3 vel = Random.onUnitSphere * Spawner.S.velocity;
+        rigid.velocity = vel;
+
+        LookAhead();
+
+        // Give the Boid a random color, but make sure it's not too dark
+        Color randColor = Color.black;
+        while ( randColor.r + randColor.g + randColor.b < 1.0f ) {
+            randColor = new Color(Random.value, Random.value, Random.value);
+        }
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach ( Renderer r in rends ) {
+            r.material.color = randColor;
+        }
+        TrailRenderer tRend = GetComponent<TrailRenderer>();
+        tRend.material.SetColor("_TintColor", randColor);
     }
 
-    // Update is called once per frame
-    void Update()
+    void LookAhead()
     {
-        
+        // Orients the Boid to look at the direction it's flying.
+        transform.LookAt(pos + rigid.velocity);
+    }
+
+    public Vector3 pos {
+        get { return transform.position; }
+        set { transform.position = value; }
     }
 }
